ee2ffe080f9b6f0a3cf1d212b00be12c4cc917de
Ludvig Qerraxhija <l.qerraxhija@gmail.com>
2025-06-26 11:18:01 +0200

    local storage - added temporarily for persistent data

--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -3,12 +3,20 @@ import { BaseTask, Task } from '../core/task.model';
 import { DUMMY_TASKS } from './dummy-tasks';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
   private _tasks = signal<Task[]>(DUMMY_TASKS);
 
   readonly tasks = this._tasks.asReadonly();
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks !== null) {
+      this._tasks.set(JSON.parse(tasks));
+    }
+  }
 
   addTask(taskData: BaseTask, userId: string) {
     this._tasks.set([
@@ -19,11 +27,17 @@ export class TaskService {
         summary: taskData.summary,
         dueDate: taskData.dueDate,
       },
-      ...this._tasks()
-    ])
+      ...this._tasks(),
+    ]);
+    this.saveTasks();
   }
 
   removeTask(id: string) {
-    this._tasks.set(this._tasks().filter((task) => task.id !== id))
+    this._tasks.set(this._tasks().filter((task) => task.id !== id));
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this._tasks()));
   }
 }
